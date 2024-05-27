c146304966233f6ad3ef0242cf56a7daeacf9658
hernando tsutsumi <hernando.tsutsumi@gmail.com>
2024-05-27 23:42:45 +0900

    code added

--- experiment.rb
+++ experiment.rb
@@ -11,6 +11,9 @@ end
 def belongs_to?(a_string, a_word)
   # TODO: return true if a_string contains a_word
   # example: belongs_to?("hey jude", "jude") => true
+  a_string.include?(a_word)
+  
+  return 
   
 end
 
